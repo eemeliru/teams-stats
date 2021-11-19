2d600de32cd87463473a3bd51498797062d94831
Eemeli R <eemeliruohio@gmail.com>
2021-11-19 10:48:29 +0200

    if name == main to anonGen

--- anonGen.py
+++ anonGen.py
@@ -3,15 +3,18 @@ import pandas as pd
 from mimesis import Person
 person = Person('fi')
 
+def main():
+    df = pd.read_csv('AllDataPreprocessed.csv')
+    df['Duration'] = pd.to_numeric(df['Duration'], downcast='float')
 
-df = pd.read_csv('AllDataPreprocessed.csv')
-df['Duration'] = pd.to_numeric(df['Duration'], downcast='float')
+    arr = df['Full Name'].unique()
 
-arr = df['Full Name'].unique()
+    for name in arr:
+        anon = person.full_name()
 
-for name in arr:
-    anon = person.full_name()
+        df.loc[df['Full Name'] == name, ['Full Name']] = anon
 
-    df.loc[df['Full Name'] == name, ['Full Name']] = anon
+    df.to_csv('anonData.csv', index=False)
 
-df.to_csv('anonData.csv', index=False)
+if __name__ == '__main__':
+    main()
