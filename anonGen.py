ee98d57d4fb67a08781c8f2b8bd84938d521a02f
eemeliru <eemeliruohio@gmail.com>
2021-11-19 10:00:10 +0200

    + anonGen.py

---
+++ anonGen.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+from mimesis import Person
+person = Person('fi')
+
+
+df = pd.read_csv('AllDataPreprocessed.csv')
+df['Duration'] = pd.to_numeric(df['Duration'], downcast='float')
+
+arr = df['Full Name'].unique()
+
+for name in arr:
+    anon = person.full_name()
+
+    df.loc[df['Full Name'] == name, ['Full Name']] = anon
+
+df.to_csv('anonData.csv', index=False)
